bedecae282083ae680d123c9c7b5d5123ab83ff6
Moin <cuebix-developer@outlook.com>
2024-10-23 03:36:35 +0500

    update working.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
@@ -11,12 +13,16 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatCardModule,
     MatIconModule,
     MatTabsModule,
+    MatProgressBarModule,
+    MatTooltipModule,
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    MatTabsModule
+    MatTabsModule,
+    MatProgressBarModule,
+    MatTooltipModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
--- src/app/mats-home/mats-home.component.ts
+++ src/app/mats-home/mats-home.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mats-home',
   templateUrl: './mats-home.component.html',
   styleUrl: './mats-home.component.scss',
-  imports: [MaterialModule],
+  imports: [MaterialModule, CommonModule],
   standalone: true
 })
 export class MatsHomeComponent {
 
-  // constructor(){
+  offersData = [
+    { icon: 'done_all', desc: 'MORE THAN 10 YEARS OF EXPERIENCE' },
+    { icon: 'done_all', desc: 'SHORT ARRIVAL TIME OF 20 MINUTES OR LESS' },
+    { icon: 'done_all', desc: 'HONEST COMPETITIVE PRICES - ZERO HIDDEN FEES' },
+    { icon: 'done_all', desc: 'FRIENDLY AND PROFESSIONAL SERVICE' },
+    { icon: 'done_all', desc: 'AVAILABLE 24 HOURS A DAY, 7 DAYS A WEEK' },
+    { icon: 'done_all', desc: 'PACKAGING AND STORAGE, OR DOOR-TO-DOOR DELIVERY' },
+  ];
 
-  // }
 }
