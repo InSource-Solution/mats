bf7bb74cc408e98227f0da26b083e37c59c3fc58
Moin <cuebix-developer@outlook.com>
2024-10-24 03:17:24 +0500

    update changes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
@@ -15,6 +18,9 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatTabsModule,
     MatProgressBarModule,
     MatTooltipModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule
   ],
   exports: [
     MatButtonModule,
@@ -22,7 +28,10 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatIconModule,
     MatTabsModule,
     MatProgressBarModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
--- src/app/mats-home/mats-home.component.ts
+++ src/app/mats-home/mats-home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../material.module';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mats-home',
@@ -9,8 +10,10 @@ import { CommonModule } from '@angular/common';
   imports: [MaterialModule, CommonModule],
   standalone: true
 })
-export class MatsHomeComponent {
+export class MatsHomeComponent implements OnInit {
 
+  userModel: any = {};
+  iframeSrc: SafeResourceUrl | undefined;
   offersData = [
     { icon: 'done_all', desc: 'MORE THAN 10 YEARS OF EXPERIENCE' },
     { icon: 'done_all', desc: 'SHORT ARRIVAL TIME OF 20 MINUTES OR LESS' },
@@ -19,5 +22,22 @@ export class MatsHomeComponent {
     { icon: 'done_all', desc: 'AVAILABLE 24 HOURS A DAY, 7 DAYS A WEEK' },
     { icon: 'done_all', desc: 'PACKAGING AND STORAGE, OR DOOR-TO-DOOR DELIVERY' },
   ];
+
+  constructor(private sanitizer: DomSanitizer){
+
+  }
+
+  ngOnInit(): void {
+    this.userModel['name'] = "Umair";
+    this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.google.com/maps/place/MATS+Towing+%26+Transport/@24.930128,67.0661286,17z/data=!3m1!4b1!4m6!3m5!1s0x3eb33f03da43bd75:0x54b7c34c6ec61368!8m2!3d24.9301232!4d67.0687035!16s%2Fg%2F11kbxcmkmm?entry=ttu&g_ep=EgoyMDI0MTAyMS4xIKXMDSoASAFQAw%3D%3D');
+  }
+
+  onEmailChange(){
+    if(!this?.userModel?.email?.includes('@gmail.com')){
+      this.userModel.isError = true;
+    } else {
+      this.userModel.isError = false;
+    }
+  }
 
 }
